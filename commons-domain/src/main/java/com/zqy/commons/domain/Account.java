a06d4fadec7edb23abe114ad1b4365433e8306b1
zhangqianyang <yqzhang@storeteam.cn>
2020-05-25 18:36:38 +0800

    通用的领域模型

---
+++ commons-domain/src/main/java/com/zqy/commons/domain/Account.java
@@ -0,0 +1,43 @@
+package com.zqy.commons.domain;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "account")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer balance;
+
+    /**
+     * @return id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return balance
+     */
+    public Integer getBalance() {
+        return balance;
+    }
+
+    /**
+     * @param balance
+     */
+    public void setBalance(Integer balance) {
+        this.balance = balance;
+    }
+}
