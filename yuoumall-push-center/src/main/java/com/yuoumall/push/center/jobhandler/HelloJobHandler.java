9175bd357544c9d6a6f79462271d430453f75025
yqzhang@storeteam.cn <yqzhang@storeteam.cn>
2020-07-02 14:11:10 +0800

    请求完善

--- yuoumall-push-center/src/main/java/com/yuoumall/push/center/jobhandler/HelloJobHandler.java
+++ yuoumall-push-center/src/main/java/com/yuoumall/push/center/jobhandler/HelloJobHandler.java
@@ -7,6 +7,11 @@ import com.xxl.job.core.handler.annotation.XxlJob;
 import com.xxl.job.core.log.XxlJobLogger;
 import com.yuoumall.push.center.bo.Admin;
 import com.yuoumall.push.center.service.AdminService;
+import com.yuoumall.push.center.util.Httputil;
+
+import org.json.JSONArray;
+
+import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -18,18 +23,38 @@ public class HelloJobHandler extends IJobHandler {
 
     @Override
     @XxlJob("SD001")
-    public ReturnT<String> execute(String param)throws Exception {
-        XxlJobLogger.log("XXL-JOB, Join Method param :" + param);
-        System.out.println("XXL-JOB, Join Method. param : " + param);
+    public ReturnT<String> execute(String param) throws Exception {
+        Long startTs = System.currentTimeMillis();
+        XxlJobLogger.log("XXL-JOB, Join Method param :" + param + " 任务开始时间:" + startTs);
+        System.out.println("XXL-JOB, Join Method. param : " + param + " 任务开始时间:" + startTs);
         Admin admin = new Admin();
         try {
             admin = adminService.selectByPrimaryKey(1);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        XxlJobLogger.log("XXL-JOB, get object :" + admin.toString());
-        System.out.println("XXL-JOB, get object :" + admin.toString());
-        return new ReturnT(admin);
+        XxlJobLogger.log("XXL-JOB, 获取数据源:" + admin.toString());
+        System.out.println("XXL-JOB, 获取数据源:" + admin.toString());
+
+        String PoUrl = "http://sap-pod.yuou.com:50000/RESTAdapter/SD001";
+        XxlJobLogger.log("XXL-JOB 开始请求PO连接：" + PoUrl);
+        System.out.println("XXL-JOB 开始请求PO连接：" + PoUrl);
+
+        // 请求po
+        JSONObject jsonObject = JSONObject.fromObject(admin);
+        String result = Httputil.httpPostWithjson(PoUrl, jsonObject.toString());
+
+        XxlJobLogger.log("XXL-JOB, 获取到的PO返回参数 :" + result);
+        System.out.println("XXL-JOB, 获取到的PO返回参数 : " + result);
+
+        JSONArray jsonArray = new JSONArray(result);
+
+        Long endTs = System.currentTimeMillis();
+
+        XxlJobLogger.log("XXL-JOB, 耗时 :" + (endTs - startTs));
+        System.out.println("XXL-JOB, 耗时 : " + (endTs - startTs));
+
+        return new ReturnT(jsonArray.toString());
     }
 
 }
